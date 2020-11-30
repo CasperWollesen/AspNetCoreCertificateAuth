69de15c63d121a1b24dade5b49e6f698baad0042
Casper Wollesen <CasperWollesen@GMail.com>
2020-11-30 14:15:01 +0100

    Revert "Tests"
    This reverts commit 6cd60bfaad07f079cf0c859efd3203046f3de28c.

--- AspNetCoreCertificateAuthApiSelfSigned/Controllers/ValuesController.cs
+++ AspNetCoreCertificateAuthApiSelfSigned/Controllers/ValuesController.cs
@@ -10,7 +10,7 @@ namespace AspNetCoreCertificateAuthApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    // [Authorize]
+    [Authorize]
     public class ValuesController : ControllerBase
     {
         // GET api/values
