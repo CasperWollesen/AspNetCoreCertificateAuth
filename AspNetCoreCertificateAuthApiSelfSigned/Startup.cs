fab5a618ee21e85d3a62a4dc7fd308de062d7fb0
Casper Wollesen <CasperWollesen@GMail.com>
2020-11-30 14:14:57 +0100

    Revert "C"
    This reverts commit 92af1e323f82af7f63d3753ecbe038a4f2e06546.

--- AspNetCoreCertificateAuthApiSelfSigned/Startup.cs
+++ AspNetCoreCertificateAuthApiSelfSigned/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+
 namespace AspNetCoreCertificateAuthApi
 {
     public class Startup
